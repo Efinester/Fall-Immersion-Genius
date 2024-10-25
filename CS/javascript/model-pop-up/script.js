e75531dc5dcf3f26a159dda8dad672817d5c7e69
Efinester <efrayim.davis@hgs.hiddengeniusproject.org>
2024-10-25 17:23:41 -0400

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,11 @@
-function modelPopUp() {
-    // Your code here.
+document.addEventListener("DOMContentLoaded",  () => {
+    const modal = document.getElementById("myModal");
+    openModalButten.addEventListener["click", () => {
+        modal.style.display = "block";
+
+     closeModalButten.addEventListener["click", () => {
+        modal.style.display = "block";
+
+         window.addEventListener("clilck", (event) => {
+    
 }   
